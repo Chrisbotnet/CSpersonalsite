cd81dd80b021b1242197e18d8d969161c0128e2c
TheChrisBot <chrisstrother@hotmail.com>
2023-04-06 18:01:13 -0700

    Working on adding the contact form and email.

---
+++ javascript.js
@@ -0,0 +1,24 @@
+console.log("Hello There!");
+//Below is my attempt at trying to get the contact page to send emails... so far it is not working!
+function sendmail(){
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+
+ const serviceID = "service_yjn3nd9";
+ const templateID = "template_kiurs6c";
+
+ emailjs.send(serviceID, templateID, params)
+ .then(res=>{
+        document.getElementById("fname").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!")
+})
+.catch(err=>console.log(err));
+}
+
