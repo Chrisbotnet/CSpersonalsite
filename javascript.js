533203c51dc7a9814f9878c03e7744782227b768
TheChrisBot <chrisstrother@hotmail.com>
2023-04-13 19:14:48 -0700

    fixed link on the contact page and added some selectors on the portfolio page

--- javascript.js
+++ javascript.js
@@ -1,21 +1,26 @@
 console.log("Hello There!");
 //Below is my attempt at trying to get the contact page to send emails... so far it is not working!
 
-   const form = document.querySelector("#form")
-   const submitButton = document.querySelector("#submit")
-   const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+   const form = document.querySelector("#form");
+   const submitButton = document.querySelector("#submit");
+   const scriptURL = "https://httpbin.org/post";
 
- const serviceID = "service_yjn3nd9";
- const templateID = "template_kiurs6c";
+   form.addEventListener("submit", (e) => {
+     submitButton.disabled = true;
+     e.preventDefault();
+     let requestBody = new FormData(form);
+     fetch(scriptURL, { method: "POST", body: requestBody })
+       .then((response) => {
+         alert("Success!", response);
+         submitButton.disabled = false;
+         location.href= "finalform.html";
 
- emailjs.send(serviceID, templateID, params)
- .then(res=>{
-        document.getElementById("fname").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!")
-})
-.catch(err=>console.log(err));
-}
+
+       })
+       .catch((error) => {
+         alert("Error!", error.message);
+         submitButton.disabled = false;
+         location.href = "finalform.html";
+       });
+   });
 
